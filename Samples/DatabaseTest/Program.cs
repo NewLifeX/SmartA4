c9fdc0833ae2d1764fbafb89f166f6b12289b630
大石头 <nnhy@vip.qq.com>
2023-10-28 23:33:28 +0800

    A4工业计算机增加一系列例程代码

---
+++ Samples/DatabaseTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using DatabaseTest.Data;
+using NewLife.Log;
+using NewLife.Serialization;
+
+XTrace.UseConsole();
+
+// 新增用户插入数据库
+var user = new Member
+{
+    Name = "Stone",
+    DisplayName = "大石头",
+    Password = "123456",
+    Enable = true
+};
+user.Insert();
+
+// 查询用户
+var user2 = Member.FindByName("Stone");
+XTrace.WriteLine("FindByName: {0}", user2.ToJson(true));
+
+// 更新用户
+user2.DisplayName = "智能大石头";
+user2.Update();
+
+var user3 = Member.FindById(user2.Id);
+XTrace.WriteLine("FindByID: {0}", user3.DisplayName);
+
+// 删除用户
+user3.Delete();
---
+++ Samples/LedTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using SmartA4;
+
+var led = new OutputPort { FileName = "/dev/led" };
+
+while (true)
+{
+    // 灭
+    led.Write(false);
+
+    Thread.Sleep(500);
+
+    // 亮
+    led.Write(true);
+
+    Thread.Sleep(500);
+}
